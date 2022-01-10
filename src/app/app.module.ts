189498d4e411ba8f5e77402876e11f9a4c42380c
Karol Gaj <karol.gaj93@gmail.com>
2022-01-10 12:12:28 +0100

    init work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import "@ui5/webcomponents/dist/Button";
+import "@ui5/webcomponents/dist/Label";
+import "@ui5/webcomponents/dist/Table.js";
+import "@ui5/webcomponents/dist/TableColumn.js";
+import "@ui5/webcomponents/dist/TableRow.js";
+import "@ui5/webcomponents/dist/TableCell.js";
+
+import "@ui5/webcomponents-fiori/dist/Bar";
+
+import "@ui5/webcomponents-icons/dist/AllIcons.js"
+
+import '@ui5/webcomponents/dist/Assets';
+import '@ui5/webcomponents-fiori/dist/Assets';
+// @ts-ignore
+import {setTheme} from '@ui5/webcomponents-base/dist/config/Theme';
+
+
+
+setTheme('sap_belize')
+
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
